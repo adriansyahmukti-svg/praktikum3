427668d6347e469de460462e8520cee4f24fb9eb
adriansyahmukti-svg <adriansyahmukti@gmail.com>
2025-10-17 19:18:09 +0700

    Update kasir.py

--- praktikum 3/program kasir sederhana/kasir.py
+++ praktikum 3/program kasir sederhana/kasir.py
@@ -1,10 +1,7 @@
-# ===============================================
-# Program Kasir Sederhana (Tanpa Input Manual)
-# ===============================================
 
 print("=== Program Kasir Sederhana ===\n")
 
-# Data sudah ditentukan langsung di dalam program
+# Data sudah ditentukan 
 harga = 20000          # Harga per unit barang
 jumlah = 2             # Jumlah barang
 diskon_persen = 10     # Diskon tetap 10%
@@ -21,3 +18,4 @@ print(f"Jumlah barang        : {jumlah}")
 print(f"Total sebelum diskon : Rp {total}")
 print(f"Diskon ({diskon_persen}%)        : Rp {int(diskon)}")
 print(f"Total setelah diskon : Rp {int(total_akhir)}")
+
